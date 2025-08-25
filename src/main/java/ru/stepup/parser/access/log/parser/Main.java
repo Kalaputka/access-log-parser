f87181f299dd33217b6e21799889c7d4777fcb52
Alexander Kazakov <ASKazakov@inno.tech>
2025-08-25 15:17:11 +0400

    Изменил класс Main, добавил ввод 2-х переменных и отображение арифметических операций с ними

--- src/main/java/ru/stepup/parser/access/log/parser/Main.java
+++ src/main/java/ru/stepup/parser/access/log/parser/Main.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println(firstNumber+secondNumber);
+        System.out.println(firstNumber-secondNumber);
+        System.out.println(firstNumber*secondNumber);
+        System.out.println((double) firstNumber/secondNumber);
     }
 }
