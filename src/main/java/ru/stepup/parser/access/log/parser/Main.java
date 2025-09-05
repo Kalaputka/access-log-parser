72214cb8ec2d82e88448f2ef7a82fca984e2e129
Alexander Kazakov <ASKazakov@inno.tech>
2025-09-06 02:06:52 +0400

    Добавил в класс Main Задание по теме "Циклы"

--- src/main/java/ru/stepup/parser/access/log/parser/Main.java
+++ src/main/java/ru/stepup/parser/access/log/parser/Main.java
@@ -1,16 +1,33 @@
 package ru.stepup.parser.access.log.parser;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма чисел:" + (firstNumber+secondNumber));
-        System.out.println("Разность чисел:" + (firstNumber-secondNumber));
-        System.out.println("Произведение чисел:" + firstNumber*secondNumber);
-        System.out.println("Частное чисел:" + (double) firstNumber/secondNumber);
+        Scanner scanner = new Scanner(System.in);
+        int correctFileCount = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Указанный файл не существует");
+                } else {
+                    System.out.println("Указанный путь ведет к папке");
+                }
+                continue;
+            }
+
+            correctFileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + correctFileCount);
+        }
     }
 }
